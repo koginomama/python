e02c7fd7aac282bd26dd2b966664adc130ff451b
koginomama <konomamadeha@gmail.com>
2023-08-16 22:46:30 +0900

    sutdy8_16

--- font_families.py
+++ font_families.py
@@ -1,4 +1,9 @@
-import tkinter
-import tkinter.font
-root = tkinter.Tk()
-print(tkinter.font.families())
+# import tkinter
+# import tkinter.font
+# root = tkinter.Tk()
+# print(tkinter.font.families())
+
+for i in range(1, 10):
+    for j in range(1, 10):
+        result = i * j
+        print(f"{i} × {j} = {result}")
